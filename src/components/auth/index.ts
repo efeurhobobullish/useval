297a3d3be5b9a5d0b106dd28c3480d7b8f6b5ce6
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-10 11:15:11 +0100

    Update index.ts

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,2 +1,3 @@
 export { default as NewAccount } from "./new-account";
 export { default as ExistingAccount } from "./existing-account";
+export { default as VerifyAccount } from "./verify-account";
