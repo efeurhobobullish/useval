e448718ff821e017ecad1784bd8bd5c1425022ed
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-11 20:00:51 +0100

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -24,11 +24,6 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      window.location.href = "/";
-    }
-
     return Promise.reject(error);
   }
 );
