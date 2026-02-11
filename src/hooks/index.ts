97fea6736fa78388c3c30f81dcb45abdd40806e0
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-11 12:32:29 +0100

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
 export {default as useTheme} from "./useTheme";
 export {default as useAuth} from "./useAuth";
 export {default as useWalletDashboard} from "./useWalletDashboard";
-export {default as useMyValentines} from "./useMyValentines";
+export {default as useValentines} from "./useValentines";
