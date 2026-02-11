32d8a1a895e3fa1fa2eab27da4115f74ff1fbee3
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-11 16:57:13 +0100

    Create useFundWallet.ts

---
+++ src/hooks/useFundWallet.ts
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import api from "@/config/api";
+
+const useFundWallet = () => {
+  const [loading, setLoading] = useState(false);
+
+  const fundWallet = async (amount: number) => {
+    setLoading(true);
+    try {
+      const res = await api.post("/v1/wallet/fund", { amount });
+      return res.data;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    fundWallet,
+    loading,
+  };
+};
+
+export default useFundWallet;
