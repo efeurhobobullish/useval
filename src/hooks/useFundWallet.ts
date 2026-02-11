00df8f368334cf025c2cd6fb0a1c4ccc5af14a40
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-11 17:11:49 +0100

    Update useFundWallet.ts

--- src/hooks/useFundWallet.ts
+++ src/hooks/useFundWallet.ts
@@ -1,29 +1,20 @@
 import { useState } from "react";
 import api from "@/config/api";
 
-type FundPayload = {
-  fullName: string;
-  phone: string;
-  amount: number;
-};
-
 const useFundWallet = () => {
   const [loading, setLoading] = useState(false);
 
-  const fundWallet = async (payload: FundPayload) => {
+  const fundWallet = async (amount: number) => {
     setLoading(true);
     try {
-      const res = await api.post("/v1/wallet/fund", payload);
+      const res = await api.post("/v1/wallet/fund", { amount });
       return res.data;
     } finally {
       setLoading(false);
     }
   };
 
-  return {
-    fundWallet,
-    loading,
-  };
+  return { fundWallet, loading };
 };
 
 export default useFundWallet;
