79a9764794ca753c6f79ce4be37cfab4008728ea
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-02-24 10:36:09 +0100

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 /* ================================
    BASE CONFIGURATION
 ================================ */
-const API_URL = "https://useval-backend-2ab4e507f60f.herokuapp.com";
+const API_URL = "https://useval-backend-6982418bc6a0.herokuapp.com";
 
 const api = axios.create({
   baseURL: API_URL,
