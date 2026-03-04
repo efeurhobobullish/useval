fa6c0ce7542b1847d75b2100852adf73cff63bc3
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-03-04 09:28:05 +0100

    Delete backend directory

--- backend/src/routes/otp.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { sendOtp, verifyOtp } from "../controllers/otp.controller.js";
-
-const otpRouter = Router();
-
-otpRouter.post("/send", sendOtp);
-otpRouter.post("/verify", verifyOtp);
-
-export default otpRouter;
